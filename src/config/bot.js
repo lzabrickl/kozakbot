f2e61bc7650673f6bc55f31a99d2cea035a694a7
lzabrickl <corwin.kidwell@gmail.com>
2026-06-14 17:04:28 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "BOA KOZAK BOA KOZAK BOA KOZAK",
         // Activity type number (0 = Playing).
         type: 3, 
       },
